2911618ba84f742b6888a2e55b59a15a7527839a
Danilo Argolo <dan_argolo@hotmail.com>
2022-10-21 19:20:48 -0300

    Requisito 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
-      </div>
+      <>
+        <div data-testid="missions">
+          <Title headline="Missões" />
+        </div>
+        <div className="missions">
+          { missions.map((mission, index) => (<MissionCard
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+            key={ `${index}-${mission.name}` }
+          />))}
+        </div>
+      </>
     );
   }
 }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -10,11 +10,13 @@ class SolarSystem extends React.Component {
         <div data-testid="solar-system">
           <Title headline="Planetas" />
         </div>
-        { planets.map((planet) => (<PlanetCard
-          planetName={ planet.name }
-          planetImage={ planet.image }
-          key={ planet.name }
-        />))}
+        <div className="planets">
+          { planets.map((planet) => (<PlanetCard
+            planetName={ planet.name }
+            planetImage={ planet.image }
+            key={ planet.name }
+          />))}
+        </div>
       </>
     );
   }
